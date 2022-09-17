0cb2b04ea993b2a255a0bea548da3c53747c5509
Like4Schnitzel <78065512+Like4Schnitzel@users.noreply.github.com>
2022-09-17 20:10:07 +0200

    Added the project files

---
+++ README.md
@@ -0,0 +1,45 @@
+This is a Discord bot, that gets and displays information about a Minecraft Server, using the Minecraft Server Status API (https://api.mcsrvstat.us/).
+By standard it updates it's information every minute, but you may change this in envs.json (more information below).
+In it's status it displays the IP of the server and the current number of players, if the server is online. Otherwise, it will say that the server is offline.
+Everytime a change in player count is detected, it will send an update message in a specified channel.
+The update message contains the IP of the server, online status, and a list of players currently online.
+HOW TO USE
+---
+In Discord, go to your User Settings, select "Advanced" and enable "Developer Mode"
+Open a web browser of your choice and go to https://discord.com/developers/applications. 
+Click the "New Application" button in the upper right corner, enter a Name (any will do), click the checkmark and choose "Create".
+Navigate to the "Bot" section in the menu on the left side, then click the "Add Bot" button and confirm.
+Choose "Reset Token", follow the steps and copy the new one to the "TOKEN" field in env.json
+Scroll down to "Bot Permissions" and choose the following: "Send Messages", "Manage Messages" and "Read Message History".
+After that, in the menu on the lefthand side, open the "OAuth2" section and go to "URL Generator".
+For "Scopes" choose "bot", then for "Bot Permissions" select the same ones as earlier.
+Then open the link at the bottom in your browser.
+Choose a Server to add it to from the dropdown menu, and that part is done.
+env.json
+---
+-TOKEN-
+As mentioned earlier, "TOKEN" is where you put the token of the discord bot.
+-CHANNEL_ID-
+This is for specifying which channel the update messages should get sent to. 
+You can get the ID of a channel by right clicking it and selecting "Copy ID", if you have Developer Mode enabled.
+-SERVER_ID-
+Same as CHANNEL_ID, but replace "channel" with "server".
+-IP-
+This is for specifying the IP which the server runs on.
+If you plan to run this on the same machine or network as the server, you may leave it empty and the bot will autodetect your external IP.
+If your external IP changes, while the server is running, it will try to detect your new one.
+This also means that you can use it to keep the people playing on your server updated with your current IP, if you have a dynamic one.
+-DELETE_MESSAGES-
+By default, every time the bot sends a Status Update, it will check the last 100 messages in the channel, and delete any that have been sent by it.
+This is to prevent flooding of a channel with Status Updates.
+You may change the number of messages it looks through, or disable it entirely by setting the variable to 0.
+-DELAY_MINS-
+This variable is for specifying how many minutes the bot should wait before updating it's information again.
+Final Steps
+---
+Make sure you have python 3 installed, if not, install it.
+Run the following commands to install python libraries:
+pip install discord.py
+pip install requests
+Then, put bot.py and env.json in any directory, just make sure they're in the same one.
+Finally, run bot.py with "python3 bot.py" (or some other way of running a python script) and you're done.
---
+++ bot.py
@@ -0,0 +1,96 @@
+from discord.ext import tasks
+import discord
+from discord.ext import commands
+from requests import get
+from json import loads
+from json import load
+
+#loading env.json
+f = open('env.json')
+envs = load(f)
+
+#setting the bot token
+TOKEN = envs['TOKEN']
+#setting IP to the one provided in envs.json
+IP = envs['IP']
+#if none has been provided, setting IP to the external IP of the machine this runs on
+if IP == "":
+    IP = get('https://api.ipify.org').content.decode('utf8')
+#making global variables for the value of players for the current and previous loop
+LAST_PLAYERS = "0"
+CURRENT_PLAYERS = "0"
+#variables to store contents of env.json so that it can be closed later
+SERVER_ID = envs['SERVER_ID']
+CHANNEL_ID = envs['CHANNEL_ID']
+DELETE_MESSAGES = envs['DELETE_MESSAGES']
+DELAY_MINS = envs['DELAY_MINS']
+IP_provided = (envs['IP'] != "")
+
+#initializing discord client
+client = discord.Client(intents=discord.Intents.default())
+
+#making the code loop every minute
+@tasks.loop(minutes=DELAY_MINS)
+async def get_server_info():
+    #making the global variables writable
+    global LAST_PLAYERS
+    global CURRENT_PLAYERS
+    global IP
+    #settign LAST_PLAYERS to the
+    LAST_PLAYERS = CURRENT_PLAYERS
+    #fetching guild (server) and channel
+    guild = await client.fetch_guild(SERVER_ID)
+    channel = await guild.fetch_channel(CHANNEL_ID)
+    #makes a request to the API
+    server_data = loads(get("https://api.mcsrvstat.us/2/" + IP).text)
+    #pre-emptively sets the status to offline
+    online_string = "offline"
+    #check if the API returns, that the server is online
+    if server_data['online']:
+        #if it is, get number of players and add them to the string. Also change offline to online.
+        CURRENT_PLAYERS = str(server_data['players']['online'])
+        online_string = CURRENT_PLAYERS + " players online"
+    #check if there's been a change in player count
+    if(int(CURRENT_PLAYERS) != int(LAST_PLAYERS)):
+        #check if messages should be deleted
+        if DELETE_MESSAGES > 0:
+            #check if there have been any previous messages sent by the bot within the past x messages
+            async for message in channel.history(limit=DELETE_MESSAGES):
+                #check if message author is the bot
+                if message.author == client.user:
+                    #if yes, delete the message
+                    await message.delete()
+        #check if API returned offline
+        if online_string != "offline":
+            #if not, work on the online Status Update
+            message = "```\nServer Status Update:\nIP: " + IP + " | \U0001F7E2 Online\nCurrent Players:"
+            #add all player names to the Update
+            for player in server_data['players']['list']:
+                message += "\n-) " + player
+            message += "\n```"
+            #send the Update
+            await channel.send(message)
+        #do this if the API returns offline
+        else:
+            #send offline Status Update
+            await channel.send("```\nServer Status Update:\nIP: " + IP + " | \U0001F534 Offline\n```")
+    #check if server is offline
+    if online_string == "offline":
+        #in case this was caused by a change in IP, get external IP again, if none has been provided
+        if not IP_provided:
+            IP = get('https://api.ipify.org').content.decode('utf8')
+    #change Bot status
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=IP + " | " + online_string))
+
+#define function that executes once after the bot starts
+@client.event
+async def on_ready():
+    await client.wait_until_ready()
+    #run the looping function
+    get_server_info.start()
+
+#closing env.json
+f.close()
+
+#run bot
+client.run(TOKEN)
